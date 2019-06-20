4d8103489bae263998449c06144095e959d252f5
Prahlad Yeri <prahladyeri@yahoo.com>
2019-06-20 11:55:16 +0530

    chore: released 1.0.10

--- vtscan/__init__.py
+++ vtscan/__init__.py
@@ -3,6 +3,6 @@ __email__ = "prahladyeri@yahoo.com"
 __copyright__ = "(c) 2019 Prahlad Yeri"
 
 __license__ = "MIT"
-__version__ = "1.0.9"
+__version__ = "1.0.10"
 __title__ = 'VTScan'
 __description__ = 'Utility to scan for malicious files using the VirusTotal API'
