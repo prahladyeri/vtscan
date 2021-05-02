0be9275c0569cd509ac1dcc4ecb69c5e0f4c21df
Prahlad Yeri <prahladyeri@yahoo.com>
2021-05-02 18:21:47 +0530

    build: 1.0.15

--- vtscan/__init__.py
+++ vtscan/__init__.py
@@ -3,6 +3,6 @@ __email__ = "prahladyeri@yahoo.com"
 __copyright__ = "(c) 2019 Prahlad Yeri"
 
 __license__ = "MIT"
-__version__ = "1.0.14" 
+__version__ = "1.0.15"
 __title__ = 'VTScan'
 __description__ = 'Utility to scan for malicious files using the VirusTotal API'
