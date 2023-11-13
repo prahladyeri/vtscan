a1ae14205cbceee6d48cbf02c6484276fd2494f5
Prahlad Yeri <prahladyeri@yahoo.com>
2023-11-13 10:46:55 +0530

    feat: added cache feature

--- vtscan/vtscan.py
+++ vtscan/vtscan.py
@@ -3,8 +3,6 @@
 #
 # @author Prahlad Yeri<prahladyeri@yahoo.com>
 # @license MIT
-# @modified 13-05-2019
-#
 import requests
 import argparse
 import os, sys
@@ -15,6 +13,7 @@ from vtscan import __title__, __description__, __version__
 
 pkg_name = "vtscan"
 cfg = {"api_key": ""} #default
+cache = {"data": {}}
 
 #@todo: move this to a common util library
 CSI = '\033['
@@ -32,23 +31,33 @@ class Color():
 clr = Color()
 
 
-def get_config_path():
-    return os.path.join(os.path.expanduser("~/.config/"), pkg_name+'-settings.json')
+def get_config_dir():
+    #return os.path.join(os.path.expanduser("~/.config/"), pkg_name+'-settings.json')
+    tpath = os.path.expanduser("~/.config/vtscan/")
+    tpath = tpath.replace(os.sep, '/')
+    if not os.path.exists(tpath):
+        os.makedirs(tpath)
+    return tpath
 
 def load_config():
     global cfg
     #tpath = sysconfig.get_path('purelib') + os.sep + "siterank"
-    tpath = os.path.expanduser("~/.config/")
-    if not os.path.exists(tpath):
-        os.makedirs(tpath)
-    cfg_path = os.path.join(tpath, pkg_name+'-settings.json')
-    if not os.path.exists(cfg_path):
-        open(cfg_path, 'w').write(json.dumps(cfg))
+    fname = get_config_dir() + 'settings.json'
+    if not os.path.exists(fname):
+        open(fname, 'w').write(json.dumps(cfg))
     else:
-        cfg = json.loads( open(cfg_path, 'r').read())
+        cfg = json.loads( open(fname, 'r').read())
     return
-
-
+    
+def load_cache():
+    global cache
+    fname = get_config_dir() + "cache.json"
+    if not os.path.exists(fname):
+        open(fname, 'w').write(json.dumps(cache))
+    else:
+        cache = json.loads(open(fname).read())
+    return
+    
 def checkkey(kee):
     try:
         if len(kee) == 64:
@@ -84,8 +93,25 @@ def fileexists(filepath):
     except Exception as ex:
             print(ex)
 
+def print_scan(data):
+    print ("permalink: ", data.get('permalink'))
+    positives = int(data.get('positives'))
+    total = int(data.get('total'))
+    # mcafee = str(data.get('report'))
+    print("Number of positives: %d (out of %d scanners applied)" % (positives, total))
+    #print("sha1: %s" % data.get('sha256'))
+    print("verbose_msg: %s" % data.get('verbose_msg'))
+    scans = data.get('scans')
+    for key in scans:
+        if scans[key]['detected']: 
+            print(clr.RED + key, "v", scans[key]['version'], ": ", str(scans[key]['result']), clr.RESET)
+    if positives == 0:
+        print(data.get('sha1') + ' is ' + clr.GREEN + 'likely good.' + clr.RESET)
+    else:
+        print(data.get('sha1') + ' is ' + clr.RED + 'likely malicious.' + clr.RESET)
 
 def scan(hash, log_output=False, is_file=False):
+    global cache
     if cfg['api_key'] == "":
         raise Exception("api key is missing")
         return
@@ -96,8 +122,8 @@ def scan(hash, log_output=False, is_file=False):
         params = {'apikey': cfg['api_key']} #'allinfo': verbose
         files = {'file': (os.path.split(hash)[1], open(hash, 'rb'))}
         print("Uploading file...")
-        url = requests.post('https://www.virustotal.com/vtapi/v2/file/scan', params=params, files=files)
-        json_response = url.json()
+        resp = requests.post('https://www.virustotal.com/vtapi/v2/file/scan', params=params, files=files)
+        json_response = resp.json()
         #print("json_response:", json_response)
         if 'sha256' in json_response.keys():
             print("Performing scan. If it doesn't work, restart the program again")
@@ -105,8 +131,12 @@ def scan(hash, log_output=False, is_file=False):
         return
     else:
         params = {'apikey': cfg['api_key'], 'resource': hash, } #'allinfo': verbose
-        url = requests.get('https://www.virustotal.com/vtapi/v2/file/report', params=params)
-    json_response = url.json()
+        resp = requests.get('https://www.virustotal.com/vtapi/v2/file/report', params=params)
+    json_response = resp.json()
+    # store to cache if not already
+    if hash not in cache['data'].keys():
+        cache['data'][hash] = dict(json_response)
+        open(get_config_dir() + "cache.json", 'w').write(json.dumps(cache))
     if log_output:
         ss = json.dumps(json_response, indent=4)
         open('output.json','w').write(ss)
@@ -114,28 +144,12 @@ def scan(hash, log_output=False, is_file=False):
     response = int(json_response.get('response_code'))
     if response == 0:
         print (clr.YELLOW + 'Not found in VT Database' + clr.RESET)
-        return "not_found"
+        return 'not found'
     elif response == 1:
         print ('Found in VT Database')
-        print ("permalink: ", json_response.get('permalink'))
-        positives = int(json_response.get('positives'))
-        total = int(json_response.get('total'))
-        # mcafee = str(json_response.get('report'))
-        print("Number of positives: %d (out of %d scanners applied)" % (positives, total))
-        #print("sha1: %s" % json_response.get('sha256'))
-        print("verbose_msg: %s" % json_response.get('verbose_msg'))
-        if positives == 0:
-            print(clr.GREEN + hash + ' is not malicious' + clr.RESET)
-        else:
-            print(clr.RED + hash + ' is malicious' + clr.RESET)
-            print("")
-        scans = json_response.get('scans')
-        for key in scans:
-            if scans[key]['detected']: 
-                print(clr.RED + key, "v", scans[key]['version'], ": ", str(scans[key]['result']), clr.RESET)
+        print_scan(json_response)
     else:
         print (hash + ' could not be searched. Please try again later.')
-    print("")
 
 def main():
     #print('DEBUG', set(['-v', '--version']), sys.argv)
@@ -153,15 +167,16 @@ def main():
     if cfg['api_key'] == "":
         a = """VirusTotal API key is empty. To obtain an API Key:
 
-[1] Register an account on https://www.virustotal.com if you haven't. 
+[1] Register an account on https://www.virustotal.com/ if you haven't. 
 [2] Sign in and get your API Key from community profile.
 [3] Enter configuration values in %s.
 [4] Restart this program.
 
 (Refer this link for more help: https://www.virustotal.com/en/documentation/public-api/)"""
 
-        print(a % get_config_path())
+        print(a % (get_config_dir()+"settings.json"))
         return
+    load_cache()
     #check if filename is valid
     if args.input_file == None:
         print("Filename can't be empty")
@@ -173,13 +188,18 @@ def main():
         for chunk in iter(lambda: fp.read(4096), b""):
             hash.update(chunk)
     strhash = hash.hexdigest()
-    print("done. sending scan request...\n")
-    result = scan(strhash.strip(), args.log_output, False) #args.verbose
-    if result == 'not_found':
+    print("finding hash in local cache..")
+    result = False
+    if strhash in cache['data'].keys():
+        print("found in cache..\n")
+        print_scan(cache['data'][strhash])
+    else:
+        print("not found in cache, sending scan request..\n")
+        result = scan(strhash.strip(), args.log_output, False) #args.verbose
+    if result == 'not found':
         ss = input("Do you want to upload this file to vtscan database? (Y/n):")
         if ss.lower() == 'y': # @todo: make sure the file is less than 32MB
             result = scan(args.input_file, args.log_output, True)
-    print("done")
 
 # execute the program
 if __name__ == '__main__':
